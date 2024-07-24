089da7f1f647e8c75bb4a3ea1c92c17244a1ddc9
joaomarcosp-souza <joaomarcosp.sza@gmail.com>
2024-07-23 22:28:27 -0300

    teste

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home/Home';
-import Login from './pages/login/login';
+import Login from './pages/login/Login';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div>
+      <Header>
+      </Header>
       <BrowserRouter>
         <Routes>
-          <Route path='/' Component={Home} />
-          <Route path='/' Component={Login} />
+          <Route path='/home' Component={Home} />
+          <Route path='/login' Component={Login} />
         </Routes>
       </BrowserRouter>
     </div>
