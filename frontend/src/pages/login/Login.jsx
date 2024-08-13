a2b569b3626564cb6048ba9ccebd0d3a0656d463
joaomarcosp-souza <joaomarcosp.sza@gmail.com>
2024-08-12 22:19:52 -0300

    feat: link para outras páginas

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -21,11 +21,19 @@ const Login = () => {
                     <InputText id="sobrenome" type="text" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full" />
                 </div>
                 <div>
-                    <Password feedback={false} tabIndex={1} placeholder="Senha" toggleMask />
-                    <small id="username-help">Conter uma letra maiscula, numero, teste.</small>
+                    <Password feedback={false} tabIndex={1} placeholder="Senha" toggleMask/>
                 </div>
-                <div class="m-auto	">
-                    <Button label="Enviar" rounded/>
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh'}}>
+                    <Button className="btn-login" label="Enviar" severity="secondary" text raised />
+                </div>
+
+                <div class="grid">
+                    <div class="col">
+                        <Button label="Cadastrar" link onClick={() => window.open('', '_blank')} />
+                    </div>
+                    <div class="col">
+                        <Button label="Recuperar Senha" link onClick={() => window.open('', '_blank')} />
+                    </div>
                 </div>
             </Card>
         </div>
